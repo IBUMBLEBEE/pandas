fc547e7b1d62ef31390b0dcfd7bd21f9308a61fe
IBUMBLEBEE <1095037491@qq.com>
2018-01-17 14:16:26 +0800

    增加磁盘IO数据获取模块，数据来自vcenter

--- getdata.py
+++ getdata.py
@@ -12,10 +12,19 @@ import string
 from openpyxl.styles import Border, Side, PatternFill, Font, GradientFill, Alignment
 from openpyxl import Workbook
 
+from pyVmomi import vim
+from pyVim.connect import SmartConnectNoSSL, Disconnect
+import atexit
+
 
 url = "http://10.10.255.253:8080"
 user = "Admin"
 password = "zabbix!253."
+
+vcenter_host = '10.10.255.2'
+vcenter_user = 'root'
+vcenter_password = '(7lF&O14$z,K'
+vcenter_port = 443
 
 # 颜色设置
 thin = Side(border_style="thin", color="000000")
@@ -197,6 +206,78 @@ def style_range(ws, cell_range, border=Border(), fill=None, font=None, alignment
                 c.fill = fill
 
 
+def connect_vc_return_data(vc_host, vc_user, vc_password, vc_port, start, end):
+    mydata = {}
+
+    # Connect to the host without NoSSL signing
+    try:
+        si = SmartConnectNoSSL(
+            host=vc_host,
+            user=vc_user,
+            pwd=vc_password,
+            port=int(vc_port))
+        atexit.register(Disconnect, si)
+
+    except IOError as e:
+        pass
+
+    if not si:
+        raise SystemExit("Unable to connect to host with supplied info.")
+
+    content = si.RetrieveContent()
+    perfManager = content.perfManager
+
+    # create a mapping from performance stats to their counterIDs
+    # counterInfo: [performance stat => counterId]
+    # performance stat example: cpu.usagemhz.LATEST
+    # counterId example: 6
+    counterInfo = {}
+    for c in perfManager.perfCounter:
+        prefix = c.groupInfo.key
+        fullName = c.groupInfo.key + "." + c.nameInfo.key + "." + c.rollupType
+        counterInfo[fullName] = c.key
+
+    # create a list of vim.VirtualMachine objects so
+    # that we can query them for statistics
+    container = content.rootFolder
+    # viewType = [vim.VirtualMachine]
+    viewType = [vim.HostSystem]
+    recursive = True
+
+    containerView = content.viewManager.CreateContainerView(container,
+                                                            viewType,
+                                                            recursive)
+    children = containerView.view
+
+    # Loop through all the VMs
+    for child in children:
+        # Get all available metric IDs for this VM
+        print(child)
+        counterIDs = [m.counterId for m in
+                      perfManager.QueryAvailablePerfMetric(entity=child)]
+        print(counterIDs)
+        # Using the IDs form a list of MetricId
+        # objects for building the Query Spec
+        metricIDs = [vim.PerformanceManager.MetricId(counterId=c, instance="*") for c in counterIDs]
+        # print(metricIDs)
+        # Build the specification to be used
+        # for querying the performance manager
+        spec = vim.PerformanceManager.QuerySpec(entity=child,
+                                                startTime=start,
+                                                endTime=end,
+                                                metricId=metricIDs
+                                                # intervalId=7200,
+                                                )
+        # Query the performance manager
+        # based on the metrics created above
+        result = perfManager.QueryStats(querySpec=[spec])
+        for r in result:
+            for val in result[0].value:
+                if counterInfo.keys()[counterInfo.values().index(val.id.counterId)] == 'disk.usage.average':
+                    mydata['%s' % child.summary.config.name] = '%s' % str(val.value[0])
+                    return mydata
+
+
 # 控制中心
 def controller():
     multidimensional_array = []
@@ -207,11 +288,16 @@ def controller():
     time_from, time_till = handler_datetime()
 
     first_line = ["IDC-IP", "CPU-avg/GHz/month", "CPU-max/GHz/month", "MEM-pfree/%/last",
-                  "MEM-free/GB/last", "Disk-free/GB/last"]
+                  "MEM-free/GB/last", "Disk-free/GB/last", "Disk-I/O KBps/avg"]
     multidimensional_array.append(first_line)
 
-    # Disk
+    # Disk Info 计算磁盘最新剩余和磁盘I/O
+    start_time = datetime.datetime.fromtimestamp(float(time_from))
+    end_time = datetime.datetime.fromtimestamp(float(time_till))
     disk_last_data = disk_free_history_data(hid=hostids, disk_item="pfree")
+    # return data from prd vcenter
+    diskio = connect_vc_return_data(vc_host=vcenter_host, vc_user=vcenter_user, vc_password=vcenter_password,
+                                    vc_port=vcenter_port, start=start_time, end=end_time)
 
     for number in xrange(0, len(hostids)):
 
@@ -229,7 +315,8 @@ def controller():
 
         # 构建一维数组
         one_dimensional_array = (hostips[number], cpu_avg_final_data, cpu_max_final_data,
-                                 mem_pfree_final_data, mem_free_final_data, disk_last_data[number])
+                                 mem_pfree_final_data, mem_free_final_data, disk_last_data[number],
+                                 diskio['{}'.format(hostips[number])])
 
         # 构建多维数组
         multidimensional_array.append(one_dimensional_array)
